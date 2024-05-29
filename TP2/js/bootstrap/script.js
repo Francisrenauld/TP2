d3f84d1f7f911db0080c619d988a029d8b565ffb
Francis Renauld <renauldf12@gmail.com>
2024-05-29 11:28:44 -0400

    on as dequoi qui marche

--- TP2/js/bootstrap/script.js
+++ TP2/js/bootstrap/script.js
@@ -1,17 +1,6 @@
-var element = document.createElement("div");
-    element.innerHTML = 'ornggoinregnronoe4rnowe'
-document.getElementById('notes').appendChild(element).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
-var element2 = document.createElement("div");
-element2.innerHTML = 'ornggoinregnronoe4rnowe'
-document.getElementById('notes').appendChild(element2).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
-var element3 = document.createElement("div");
-    element3.innerHTML = 'ornggoinregnronoe4rnowe'
-document.getElementById('notes').appendChild(element3).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
-var element4 = document.createElement("div");
-    element4.innerHTML = 'ornggoinregnronoe4rnowe'
-document.getElementById('notes').appendChild(element4).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
-var element5 = document.createElement("div");
-    element5.innerHTML = 'ornggoinregnronoe4rnowe'
-document.getElementById('notes').appendChild(element5).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+
+/*var div = document.getElementById('notes')
+div.innerHTML += '<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">lksfdnknksfndfd</div>'*/
+
 
 
