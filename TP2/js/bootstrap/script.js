4561f63407c0caeaddca90d8d9970c8dd0290df0
Francis Renauld <renauldf12@gmail.com>
2024-05-29 10:49:22 -0400

    Note Fonctionne

---
+++ TP2/js/bootstrap/script.js
@@ -0,0 +1,17 @@
+var element = document.createElement("div");
+    element.innerHTML = 'ornggoinregnronoe4rnowe'
+document.getElementById('notes').appendChild(element).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+var element2 = document.createElement("div");
+element2.innerHTML = 'ornggoinregnronoe4rnowe'
+document.getElementById('notes').appendChild(element2).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+var element3 = document.createElement("div");
+    element3.innerHTML = 'ornggoinregnronoe4rnowe'
+document.getElementById('notes').appendChild(element3).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+var element4 = document.createElement("div");
+    element4.innerHTML = 'ornggoinregnronoe4rnowe'
+document.getElementById('notes').appendChild(element4).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+var element5 = document.createElement("div");
+    element5.innerHTML = 'ornggoinregnronoe4rnowe'
+document.getElementById('notes').appendChild(element5).classList.add('col', 'col-sm', 'col-md', 'col-lg', 'col-xl');
+
+
