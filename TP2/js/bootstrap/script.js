8c4f0ce1255c4e360ab2037ca071f74ffe83d487
Anthony McGee-Cloutier <anthomc2003@gmail.com>
2024-05-29 11:30:30 -0400

    jgru

--- TP2/js/bootstrap/script.js
+++ TP2/js/bootstrap/script.js
@@ -2,5 +2,5 @@
 /*var div = document.getElementById('notes')
 div.innerHTML += '<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">lksfdnknksfndfd</div>'*/
 
-
+form.addEventListener("submit", validateForm);
 
