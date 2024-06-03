b799917af04ad71db8807ad61c09435988762feb
Francis Renauld <renauldf12@gmail.com>
2024-06-03 17:10:48 -0400

    sd

--- TP2/js/bootstrap/script.js
+++ TP2/js/bootstrap/script.js
@@ -1,6 +1,4 @@
 'use strict';
-/*var div = document.getElementById('notes')
-div.innerHTML += '<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">lksfdnknksfndfd</div>'*/
 
 const form = document.getElementById('form')
 let array = document.querySelectorAll("li")
@@ -8,15 +6,17 @@ form.addEventListener("submit", createNote);
 
 function createNote(e) {
     
+    let colorClass = document.querySelector('input[name = color]:checked').className;
+
     if (validate()) {
         let note = document.getElementById('createNote').value
         let title = document.getElementById('title').value
         var div = document.getElementById('notes')
-        div.innerHTML += '<div id="note" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">'+'<p style="font-weight: 600;">'+title+'</p>'+'<p>'+ note +'</p>'+'</div>'
+        div.innerHTML += '<div id="note-cree" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear '+ colorClass +'">'+'<p style="font-weight: 600;">'+title+'</p>'+'<p>'+ note +'</p>'+'</div>'
         e.preventDefault()
+        console.log(colorClass)
     }
     e.preventDefault()
-
 }
 
 function validate() {
