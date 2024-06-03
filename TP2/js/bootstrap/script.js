38e14b31c49fb75dfb4f1367b5300ed08ff123bd
Francis Renauld <renauldf12@gmail.com>
2024-06-03 15:29:11 -0400

    msdosg

--- TP2/js/bootstrap/script.js
+++ TP2/js/bootstrap/script.js
@@ -10,8 +10,9 @@ function createNote(e) {
     
     if (validate()) {
         let note = document.getElementById('createNote').value
+        let title = document.getElementById('title').value
         var div = document.getElementById('notes')
-        div.innerHTML += '<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">'+ note +'</div>'
+        div.innerHTML += '<div id="note" class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 border-note-clear">'+'<p style="font-weight: 600;">'+title+'</p>'+'<p>'+ note +'</p>'+'</div>'
         e.preventDefault()
     }
     e.preventDefault()
@@ -21,10 +22,10 @@ function createNote(e) {
 function validate() {
 
     let validation = true
-    
     let text = document.getElementById('createNote').value
     let title = document.getElementById('title').value
-    if (title == '') {
+    
+    if (title.length > 16) {
         array[0].classList.remove('hide')
         validation = false
     }
